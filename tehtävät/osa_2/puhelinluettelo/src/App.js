9c5605f72bb076f3d5d343cc893c1c343fb954d8
Eero Pukkila <eero.pukkila@codento.com>
2018-06-24 21:03:36 +0300

    tehtävät 2.8 ja 2.9

--- tehtävät/osa_2/puhelinluettelo/src/App.js
+++ tehtävät/osa_2/puhelinluettelo/src/App.js
@@ -7,7 +7,9 @@ class App extends React.Component {
       persons: [
         { name: 'Arto Hellas' }
       ],
-      newName: ''
+      newName: '',
+      newNumber: '',
+      search: ''
     }
   }
 
@@ -15,10 +17,19 @@ class App extends React.Component {
     this.setState({ newName: event.target.value })
   }
 
-  addName = (event) => {
+  updateNumber = (event) => {
+    this.setState({ newNumber: event.target.value })
+  }
+
+  search = (event) => {
+    this.setState({ search: event.target.value })
+  }
+
+  addPerson = (event) => {
     event.preventDefault()
     const nameToAdd = {
-      name: this.state.newName
+      name: this.state.newName,
+      number: this.state.newNumber
     }
 
     if(this.state.persons.some(this.containsName)){
@@ -30,7 +41,8 @@ class App extends React.Component {
 
     this.setState({
       persons,
-      newName: ''
+      newName: '',
+      newNumber: ''
     })
   }
 
@@ -38,22 +50,26 @@ class App extends React.Component {
 
   render() {
 
-    const names = this.state.persons.map(person => <li key={person.name}>{person.name}</li>)
+    const filtered = this.state.persons.filter(person => person.name.toLowerCase().includes(this.state.search.toLowerCase()))
+    const persons = filtered.map(person => <li key={person.name}>{person.name}: {person.number}</li>)
 
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <form onSubmit={this.addName}>
+        <form onSubmit={this.addPerson}>
           <div>
-            nimi: <input value={this.state.newName} onChange={this.updateName}/>
+            nimi: <input value={this.state.newName} onChange={this.updateName}/> <br/>
+            numero: <input value={this.state.newNumber} onChange={this.updateNumber}/>
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
+        <h2>Haku:</h2>
+        Hae nimellä: <input value={this.state.search} onChange={this.search}/>
         <h2>Numerot</h2>
         <ul>
-          {names}
+          {persons}
         </ul>
       </div>
     )
